57756c9020736a81d0039dac6dce6ad196c3e0f9
Bianca Aparecida Alves Araujo <biancaaparecida07@gmail.com>
2023-12-27 14:10:39 -0300

    Dados da Tabela Produtos.

---
+++ Banco de Dados/populacao.sql
@@ -0,0 +1,22 @@
+
+INSERT INTO Produtos (id, nome_produto, categoria_produto) VALUES
+(1,'Produto1',4),
+(2,'Produto2',3),
+(3,'Produto3',3),
+(4,'Produto4',2),
+(5,'Produto5',2),
+(6,'Produto6',1),
+(7,'Produto7',2),
+(8,'Produto8',1),
+(9,'Produto9',4),
+(10,'Produto10',1),
+(11,'Produto11',4),
+(12,'Produto12',3),
+(13,'Produto13',3),
+(14,'Produto14',2),
+(15,'Produto15',1),
+(16,'Produto16',1),
+(17,'Produto17',5),
+(18,'Produto18',2),
+(19,'Produto19',1),
+(20,'Produto20',1);
